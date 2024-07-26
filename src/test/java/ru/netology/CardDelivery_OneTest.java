a77994225267fea4fecbf8853ebc81ceee0372cf
zdorovilov test <zdorovilovde@mail.ru>
2024-07-27 02:26:44 +0500

    Заново собрал тест

--- src/test/java/ru/netology/CardDelivery_OneTest.java
+++ src/test/java/ru/netology/CardDelivery_OneTest.java
@@ -12,24 +12,21 @@ import java.time.format.DateTimeFormatter;
 import static com.codeborne.selenide.Selenide.$;
 
 public class CardDelivery_OneTest {
-
-    private String generateDate (long addDays, String pattern) {
+    private String generateDate(long addDays, String pattern) {
         return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
     }
 
     @Test
     public void shouldBeSuccessfullyCompleted() {
-        Selenide.open("http://localhost:9999");
+        Selenide.open("http://localhost:9999/");
         $("[data-test-id='city'] input").setValue("Челябинск");
-        String planningDate = generateDate(4,"dd.mm.yyyy");
-        $("[data-test-id='date'] input]").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
-        $("[data-test-id='date'] input]").setValue(planningDate);
-        $("[data-test-id='name'] input]").setValue("Здоровилов Дмитрий");
-        $("[data-test-id='phone'] input]").setValue("+79507243850");
+        String planningDate = generateDate(4, "dd.MM.yyyy");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id='date'] input").setValue(planningDate);
+        $("[data-test-id='name'] input").setValue("Здоровилов Дмитрий");
+        $("[data-test-id='phone'] input").setValue("+79507243850");
         $("[data-test-id='agreement']").click();
         $("button.button").click();
-        $(".notification__content")
-                .shouldBe(Condition.visible, Duration.ofSeconds(15))
-                .shouldBe(Condition.text("Встреча успешно забронирована на " +planningDate));
+        $(".notification__content").shouldBe(Condition.visible, Duration.ofSeconds(15)).shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate));
     }
 }
